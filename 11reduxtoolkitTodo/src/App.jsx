ed8947390fb2c6b676a048d1defdbabefca9db6a
shaptrshi <59893262+shaptrshi@users.noreply.github.com>
2024-06-15 13:58:09 +0530

    h

---
+++ 11reduxtoolkitTodo/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import AddTodos from './components/AddTodo'
+import Todos from './components/Todos'
+
+function App() {
+  
+
+  return (
+    <>
+     <h1>Redux Toolkit</h1>
+     <AddTodos/>
+     <Todos />
+    </>
+  )
+}
+
+export default App
